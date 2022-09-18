37a923c18e5cd6a50f1d90176d7ffcc01a35fec5
Koustubh Moharir <koustubhmoharir@gmail.com>
2022-09-18 20:36:37 +0530

    Added a webpack plugin that resolves top-level lit-html imports to our own package instead.

--- packages/mythikal/.storybook/main.js
+++ packages/mythikal/.storybook/main.js
@@ -1,6 +1,21 @@
 const project = require('../webpack.config.js');
 const path = require('path');
 
+const sb = path.normalize("/@storybook/");
+class ReplaceLitHtmlPlugin {
+  apply(compiler) {
+    compiler.hooks.normalModuleFactory.tap("ReplaceLitHtmlPlugin", nmf => {
+        nmf.hooks.beforeResolve.tap("ReplaceLitHtmlPlugin", result => {
+          if (result.context.includes(sb) && result.request === "lit-html") {
+            // console.log("ReplaceLitHtmlPlugin plugin:", result);
+            result.request = "realithy"
+          }
+        });
+      }
+    );
+  }
+}
+
 module.exports = {
   "stories": [
     "../**/*.stories.mdx",
@@ -15,11 +30,12 @@ module.exports = {
     "builder": "@storybook/builder-webpack5"
   },
   webpackFinal: async (config, { configType }) => {
-    // Make whatever fine-grained changes you need
-    // Return the altered config
+    // Not sure how to combine rules
+    // console.log(config.module.rules);
     return ({
       ...config,
-      module: { ...config.module, rules: project.module.rules }
+      module: { ...config.module, rules: project.module.rules },
+      plugins: [...config.plugins, new ReplaceLitHtmlPlugin()]
     });
   },
 }
