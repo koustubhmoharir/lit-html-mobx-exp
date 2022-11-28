fa99bc28d901f3cac2518ea19c5b8409475271eb
Swanand Nirgudkar <swanand@quantumphinance.com>
2022-11-28 19:23:12 +0530

    DialogComponents: Some necessary improvements to Menu. Work started on PopupButton.

--- packages/mythikal/components/PopupButton.ts
+++ packages/mythikal/components/PopupButton.ts
@@ -3,15 +3,15 @@ import { nothing } from 'lit-html';
 import { createRef, RefOrCallback } from 'lit-html/directives/ref.js';
 import { ComponentTemplate, html, makeObservable, makeReactiveLithComponent, observable, ReactiveLithComponent, ref, RenderResult, renderTemplateContent, RepeatedTemplate, TemplateContent, unbind } from 'realithy';
 import { ComponentProps } from './Component';
-import styles from "./Menu1.module.scss";
+import styles from "./PopupButton.module.scss";
 
-interface Menu1Props<M, V, T = any> extends ComponentProps<M, V> {
-    content: TemplateContent<M, Menu1_<M, V>>;
-    items: ReadonlyArray<ComponentTemplate<M, Menu1_<M, V>, any>> | RepeatedTemplate<M, V, T, Menu1_<M, V>>;
+interface PopupButtonProps<M, V, T = any> extends ComponentProps<M, V> {
+    content: TemplateContent<M, PopupButton_<M, V>>;
+    items: ReadonlyArray<ComponentTemplate<M, PopupButton_<M, V>, any>> | RepeatedTemplate<M, V, T, PopupButton_<M, V>>;
 }
 
-class Menu1_<M, V> implements ReactiveLithComponent<M, V, Menu1Props<M, V>> {
-    constructor(readonly parent: M, readonly parentView: V, readonly props: Menu1Props<M, V>) {
+class PopupButton_<M, V> implements ReactiveLithComponent<M, V, PopupButtonProps<M, V>> {
+    constructor(readonly parent: M, readonly parentView: V, readonly props: PopupButtonProps<M, V>) {
         makeObservable(this);
     }
     readonly contentRef = createRef();
@@ -51,15 +51,6 @@ class Menu1_<M, V> implements ReactiveLithComponent<M, V, Menu1Props<M, V>> {
         if (this._isOpen && this.contentRef.value && dialogElement && !this._popper) {
             dialogElement.showModal();
             this._popper = createPopper(this.contentRef.value, dialogElement, { placement: 'bottom' });
-            const current = this;
-            dialogElement.addEventListener('click', function (event) { // TODO: Check and clean up event listener
-                let rect = dialogElement.getBoundingClientRect();
-                let isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-                if (!isInDialog) {
-                    dialogElement.close();
-                    current.close();
-                }
-            });
         }
         else if (!this._isOpen) {
             //dialogElement.close();
@@ -78,35 +69,7 @@ class Menu1_<M, V> implements ReactiveLithComponent<M, V, Menu1Props<M, V>> {
         }
     }
 }
-const menu1Comp = makeReactiveLithComponent(Menu1_);
-export function Menu1<M, V, T>(props: Menu1Props<M, V, T>): ComponentTemplate<M, V, Menu1Props<M, V, T>> {
-    return new ComponentTemplate(props, menu1Comp);
-}
-
-interface Menu1ItemProps<M, V> extends ComponentProps<M, V> {
-    onClick: (m: M, v: V) => void;
-    content: TemplateContent<M, V>
-}
-class Menu1Item_<M, V extends Menu1_<any, any>> {
-    constructor(readonly parent: M, readonly parentView: V, readonly props: Menu1ItemProps<M, V>) {
-    }
-
-    handleEvent() {
-        this.parentView.close();
-        this.props.onClick?.(this.parent, this.parentView);
-    }
-
-    render() {
-        const props = this.props;
-        const root = unbind(this.parent, this.parentView, props.root);
-        return html`
-            <li ${ref(root)} @click=${this}>
-                ${renderTemplateContent(this.parent, this.parentView, props.content)}
-            </li>
-        `;
-    }
-}
-const menu1ItemComp = makeReactiveLithComponent(Menu1Item_);
-export function Menu1Item<M, V extends Menu1_<any, any>>(props: Menu1ItemProps<M, V>): ComponentTemplate<M, V, Menu1ItemProps<M, V>> {
-    return new ComponentTemplate(props, menu1ItemComp);
+const PopupButtonComp = makeReactiveLithComponent(PopupButton_);
+export function PopupButton<M, V, T>(props: PopupButtonProps<M, V, T>): ComponentTemplate<M, V, PopupButtonProps<M, V, T>> {
+    return new ComponentTemplate(props, PopupButtonComp);
 }
